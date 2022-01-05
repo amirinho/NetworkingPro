2a8e49f4f32d0238d57a481c984ae3b105e79d56
amirinho <75517621+amirinho@users.noreply.github.com>
2022-01-05 12:07:21 +0200

    Update server.c
    Creating my server socket.

--- server.c
+++ server.c
@@ -12,3 +12,15 @@
 #include <arpa/inet.h>
 #define SIZE 1024
 #define PORT 5000
+
+
+int main()
+{
+    /*Defining variable and creating socket for my server, i choose to work with Tcp which socket argument is defined as 0 in the socket function*/
+    int server =socket (AF_INET, SOCK_STREAM, 0);
+     if (server==-1)
+    {
+       perror ("Something went wring while creating socket failed\n") ;
+        exit code (0);
+         }
+    
