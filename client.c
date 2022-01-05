e05c1bd40adaff9a2c22e6240b9803ce42e5aa16
amirinho <75517621+amirinho@users.noreply.github.com>
2022-01-05 21:33:23 +0200

    Update client.c
    get and put method for client

--- client.c
+++ client.c
@@ -34,15 +34,54 @@ int length;
  
  length = sizeof(localserver);
   
-   int b = bind(serversoc,(struct sockaddr*)&localserver, sizeof(localserver));
+   int b = connect(serversoc,(struct sockaddr*)&localserver, sizeof(localserver));
     if (b == -1) {
            perror("connecting to server failed\n");
            exit(0);
        }
 	 
        else
-           printf("Connected to client.\n");
-           
+           printf("Connected to server.\n");
+	/*method to get and put file fron and on the server*/
+
+    char get = "GET";
+    char put = "PUT";
+    int l;
+      recv (serversoc,&l, 4, 0) ;
+      char msg[l];
+      int k=recv(serversoc, msg, sizeof (msg) , 0) ;
+      msg[k]='\0';
+      printf("%s\n", msg);
+      char fname[20];
+      printf("Enter file name to request from server:");
+      scanf("%s",get, fname );
+      send (serversoc,fname, strlen(fname),0);
+      printf("File name successful sent\n");
+    
+    int p4;
+     recv(serversoc, &p4, 4, 0);
+     char msg2[p4] ;
+     int k1=recv(serversoc, msg2, sizeof (msg2) , 0);
+     msg2[k1]='\0';
+     printf("File received", msg2) ;
+     printf("In File size is %d\n",p4);
+     char fnamel[20];
+     printf("Enter new file name:");
+     scanf("%s", put, fnamel);
+     FILE *fp=fopen(fnamel,"w");
+     fwrite(msg2,1, p4, fp);
+     fclose(fp);
+     printf( "File successfuly transferred and created/n");
+    
+    
+    close(serversoc);
+       
+
+
+
+}
+      
+
        close(serversoc);
        }
       
