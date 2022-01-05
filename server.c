5ae6a107fe94e6d2a02d612de477981e6761f922
amirinho <75517621+amirinho@users.noreply.github.com>
2022-01-05 11:06:50 +0200

    Create server.c
    Defining headers and port

---
+++ server.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  amirsocketC
+//
+//  Created by admin on 1.01.2022.
+//
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#define SIZE 1024
+#define PORT 5000
